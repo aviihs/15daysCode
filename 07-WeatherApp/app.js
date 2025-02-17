4b35c911ae52574314607a2e9577bf6fe2464735
Shiva Bhusal <121562991+aviihs@users.noreply.github.com>
2025-02-17 23:06:36 +0545

    Update app.js

--- 07-WeatherApp/app.js
+++ 07-WeatherApp/app.js
@@ -6,35 +6,33 @@ let getWeather = () => {
   let cityValue = cityRef.value;
   if (cityValue.length == 0) {
     result.innerHTML = `<h3 class="msg">Please enter a city name</h3>`;
-  }
-  else {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+  } else {
+    let url = `/api/weather?city=${cityValue}`;  // Call the serverless function
+
     // Clear the input field
     cityRef.value = "";
+
     fetch(url)
       .then((resp) => resp.json())
-      // If city name is valid
       .then((data) => {
-        console.log(data);
         result.innerHTML = `
-        <h2>${data.name}</h2>
-        <h4 class="weather">${data.weather[0].main}</h4>
-        <h4 class="desc">${data.weather[0].description}</h4>
-        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-        <h1>${data.main.temp} &#176;</h1>
-        <div class="temp-container">
-            <div>
-                <h4 class="title">min</h4>
-                <h4 class="temp">${data.main.temp_min}&#176;</h4>
-            </div>
-            <div>
-                <h4 class="title">max</h4>
-                <h4 class="temp">${data.main.temp_max}&#176;</h4>
-            </div>
-        </div>
+          <h2>${data.name}</h2>
+          <h4 class="weather">${data.weather[0].main}</h4>
+          <h4 class="desc">${data.weather[0].description}</h4>
+          <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+          <h1>${data.main.temp} &#176;</h1>
+          <div class="temp-container">
+              <div>
+                  <h4 class="title">min</h4>
+                  <h4 class="temp">${data.main.temp_min}&#176;</h4>
+              </div>
+              <div>
+                  <h4 class="title">max</h4>
+                  <h4 class="temp">${data.main.temp_max}&#176;</h4>
+              </div>
+          </div>
         `;
       })
-      // If city name is invalid
       .catch(() => {
         result.innerHTML = `<h3 class="msg">City not found</h3>`;
       });
